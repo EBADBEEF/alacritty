c5257ba0e1b53f02a70720957fa142eeb1ddd116
EBADBEEF <errno@ebadf.com>
2023-04-26 11:59:29 -0700

    Add EmptySelection key binding mode
    With this, a bind can be configured to trigger based on the presence or
    absence of a selection.
    
    For example, this config emulates windows terminal style copy paste:
    ```
    [mouse]
    hide_when_typing = true
    bindings = [
    { mode = "~EmptySelection", mouse = "Right", mods = "Shift", action = "Copy" },
    { mode = "~EmptySelection", mouse = "Right", mods = "Shift", action = "ClearSelection" },
    { mode = "EmptySelection",  mouse = "Right", mods = "Shift", action = "PasteSelection" },
    ]
    ```
    
    In the above example, when no text is selected shift-rightclick will
    paste. If text is selected, then shift-rightclick will copy and clear
    the selection.

--- alacritty/src/config/bindings.rs
+++ alacritty/src/config/bindings.rs
@@ -766,11 +766,12 @@ bitflags! {
         const SEARCH                 = 0b0001_0000;
         const DISAMBIGUATE_ESC_CODES = 0b0010_0000;
         const REPORT_ALL_KEYS_AS_ESC = 0b0100_0000;
+        const EMPTY_SELECTION        = 0b1000_0000;
     }
 }
 
 impl BindingMode {
-    pub fn new(mode: &TermMode, search: bool) -> BindingMode {
+    pub fn new(mode: &TermMode, search: bool, empty_selection: bool) -> BindingMode {
         let mut binding_mode = BindingMode::empty();
         binding_mode.set(BindingMode::APP_CURSOR, mode.contains(TermMode::APP_CURSOR));
         binding_mode.set(BindingMode::APP_KEYPAD, mode.contains(TermMode::APP_KEYPAD));
@@ -785,6 +786,7 @@ impl BindingMode {
             BindingMode::REPORT_ALL_KEYS_AS_ESC,
             mode.contains(TermMode::REPORT_ALL_KEYS_AS_ESC),
         );
+        binding_mode.set(BindingMode::EMPTY_SELECTION, empty_selection);
         binding_mode
     }
 }
@@ -830,6 +832,8 @@ impl<'a> Deserialize<'a> for ModeWrapper {
                         "~vi" => res.not_mode |= BindingMode::VI,
                         "search" => res.mode |= BindingMode::SEARCH,
                         "~search" => res.not_mode |= BindingMode::SEARCH,
+                        "emptyselection" => res.mode |= BindingMode::EMPTY_SELECTION,
+                        "~emptyselection" => res.not_mode |= BindingMode::EMPTY_SELECTION,
                         _ => return Err(E::invalid_value(Unexpected::Str(modifier), &self)),
                     }
                 }
